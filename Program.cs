f7d6517b6aa84e196d7fabb92f2f92664fbfe8c3
stoneisdeveloper <m18146611430@163.com>
2025-02-01 22:56:36 +0800

    add lamda middleware

--- Program.cs
+++ Program.cs
@@ -3,12 +3,32 @@ var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
-app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Hello");
+//lamda middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello middleware 1;  ");
+    await next(context);
 });
 
-app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Hello 2");
+//middleware 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello middleware 2;  ");
+    await next(context);
 });
+
+//middleware 3
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello middleware 3;  ");
+});
+
+//app.Run(async (HttpContext context) => {
+//    await context.Response.WriteAsync("Hello 2");
+//});
+
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Hello 1");
+//});
 
 app.Run();
