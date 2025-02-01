8524c17f9de49b6e579c3ee5fad8fcaeeede67f9
stoneisdeveloper <m18146611430@163.com>
2025-02-01 22:13:55 +0800

    app.Run Test: dose't forward the request to the next

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello");
+});
+
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello 2");
+});
 
 app.Run();
